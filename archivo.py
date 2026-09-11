1356f5cf3048af8c7f1176ad95c2e26763fb8ab3
UrsulaM16 <ursulammillan406@gmail.com>
2026-09-11 13:54:28 +0200

    22

--- archivo.py
+++ archivo.py
@@ -5,8 +5,8 @@ precio_noche = float(input("Ingrese el precio por noche: "))
 total = 0
 noche = 1
 #
-while noche <= noches:  # ✅ Comparar "noche" con "noches"
-    if noche == 1:      # ✅ Comparar "noche" (la del bucle)
+while noche <= noches:  
+    if noche == 1:     
         # Primera noche: precio completo
         total += precio_noche
     else:
