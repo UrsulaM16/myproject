49e88289636326d1c30065f74395abe74231e7f7
Ursula <ursulammillan406@gmail.com>
2026-09-17 01:18:38 +0200

    Refactor Persona class and add Estudiante inheritance
    Updated class 'Persona' to include a method for full name display and added inheritance structure for 'Estudiante'.

--- ejercicios/Persona.py
+++ ejercicios/Persona.py
@@ -1,14 +1,4 @@
-"""Ejercicio 4
-Crear una clase “Persona” que sea la clase padre de otra clase 
-“Estudiante”. Por tanto:
-En la clase “Persona” su método __init__() debe de estar preparado para 
-recibir nombre y apellido. Además, esta clase , debe tener un método para 
-mostrar nombre_completo() el cual debe mostrar el nombre acompañado del 
-apellido.
-La otra clase “Estudiante”, debe de poder heredar de “Persona”, y además 
-recibir los argumentos nombre y edad. También la clase “Estudiante”, recibe
- el valor “carrera”, y además contar con un método mostrar_carrera(). 
- Las dos clases son obligatorias."""
+
 """Ejercicio 4
 Crear una clase “Persona” que sea la clase padre de otra clase 
 “Estudiante”. Por tanto:
